918f7e3b8351a0b4d1f62251e492f90dac798872
Dor Bitton <53118239+DorBitton@users.noreply.github.com>
2026-08-31 16:46:11 +0300

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.rs
@@ -0,0 +1,17 @@
+use std::collections::HashSet;
+
+impl Solution {
+    // HashSet Solution
+    pub fn has_duplicate(nums: Vec<i32>) -> bool {
+        let mut seen = HashSet::new();
+
+        for num in nums {
+            if !seen.insert(num) {
+                return true;
+            }
+        }
+        return false;
+
+        // O(n) = O(1)
+    }
+}
