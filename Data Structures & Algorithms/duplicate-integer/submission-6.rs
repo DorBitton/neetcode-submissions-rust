e3a507836f77cedb7859e3e249a156df3df9318c
Dor Bitton <53118239+DorBitton@users.noreply.github.com>
2026-08-31 14:41:40 +0300

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    // Brute force approach
+    pub fn has_duplicate(nums: Vec<i32>) -> bool {
+        for i in 0..nums.len() {
+            for j in (i + 1)..nums.len() {
+                if nums[i] == nums[j] {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
